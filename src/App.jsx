f112acdfeaa380c7edb2f5805c39fdc3a0f4fe68
Pekka Tapio Aalto <Pekka.Aalto@sasky.fi>
2026-03-09 11:26:57 +0200

    siirtää sisällön Clicker-komponentiksi

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import Balance from './components/Balance'
-import Booster from './components/Booster'
-import Header from './components/Header'
-import Lemon from './components/Lemon'
+import Clicker from './pages/Clicker'
 import Menu from './components/Menu'
 import './App.css'
 
@@ -23,12 +20,7 @@ function App() {
   return (
     <div className="root">
       <div className="root_content">
-        <div className="container clicker">
-          <Header>lemon clicker</Header>
-          <Balance total={stats.clicks} />
-          <Lemon onClick={handleClick} />
-          <Booster value={stats.increase} />
-        </div>
+        <Clicker stats={stats} handleClick={handleClick} />
       </div>
       <Menu items={stats.itemstobuy} />
     </div>
