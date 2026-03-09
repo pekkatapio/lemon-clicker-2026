2de0510974ac825e3fc898a582175e213cb1e067
Pekka Tapio Aalto <Pekka.Aalto@sasky.fi>
2026-03-09 10:20:45 +0200

    lisää Booster-komponentin

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Balance from './components/Balance'
+import Booster from './components/Booster'
 import Lemon from './components/Lemon'
 import './App.css'
 
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Balance total="157" />
         <Lemon />
+        <Booster value="3.2" />
       </div>  
     </>
   )
