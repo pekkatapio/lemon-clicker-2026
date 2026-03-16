667c593a66d64547e7b0467b2635e9368264d2bd
Pekka Tapio Aalto <Pekka.Aalto@sasky.fi>
2026-03-16 12:52:38 +0200

    lisää lukujen sievennyksen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import shortenNumber from '../utils/shortenNumber'
 import iconLemon from '../assets/lemon.svg'
 
 function Header(props) {
@@ -6,7 +7,7 @@ function Header(props) {
     return (
       <div className="header header_sub">
         <h1>{props.children}</h1>
-        <div>{props.balance} <img src={iconLemon}/></div>
+        <div>{shortenNumber(props.balance)} <img src={iconLemon}/></div>
       </div>
     )
   } else {
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,3 +1,4 @@
+import shortenNumber from '../utils/shortenNumber'
 import iconLemon from '../assets/lemon.svg'
 
 // Funktio, joka selvittää kuvan polun suoritusaikana.
@@ -15,7 +16,7 @@ function Item(props) {
       <div className="item_icon"><img src={url} alt=""/></div>
       <div className="item_desc">
         {props.item.name}<br/>
-        {props.item.price} <img src={iconLemon} alt="lemons" />
+        {shortenNumber(props.item.price)} <img src={iconLemon} alt="lemons" />
       </div>
       <div className="item_qty">{props.item.qty}</div>
     </div>
