0e4452c4f3761830ab7778d31b4b90d0717c1808
Pekka Tapio Aalto <Pekka.Aalto@sasky.fi>
2026-03-17 11:32:42 +0200

    lisää tilastoarvojen tulostuksen

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,18 @@ function App() {
   // Luodaan tilamuuttuja, johon tallennetaan tuotelista.
   const [storeitems,setStoreitems] = useState(items)
 
+  // Esitellään pelin laskennalliset alkuarvot.
+  const initialstats = {
+    clicks: 0,
+    balance: 0,
+    increase: 1,
+    itemstobuy: 0,
+    upgrades: 0,
+    collected: 0
+  }
+
   // Luodaan tilamuuttuja, johon tallennetaan pelin laskennalliset tiedot.
-  const [stats, setStats] = useState({clicks: 0, balance: 0, increase: 1, itemstobuy: 0})
+  const [stats, setStats] = useState(initialstats)
 
   // Laskee niiden tuotteiden lukumäärän, joiden ostamiseen on varaa.
   const countBuyableItems = (items, balance) => {       
@@ -67,6 +77,8 @@ function App() {
     newstats.balance = round(newstats.balance + newstats.increase,1)  
     // Lasketaan ostettavissa olevien tuotteiden lukumäärä.
     newstats.itemstobuy = countBuyableItems(storeitems,newstats.balance)
+    // Kasvatetaan kerättyjen sitruunoiden kokonausmäärää.
+    newstats.collected = round(newstats.collected + newstats.increase,1)    
     // Tallennetaan päivitetty stats-muuttuja.
     setStats(newstats); 
   }
